3001e0a44522094e26eb337d8cfb9fc58dd675db
_Nattapat.05 <natota4801@gmail.com>
2026-02-04 14:16:04 +0700

    Update calculator.c
    แก้ไขบัคให้เสร็จสมบูรณ์

--- calculator.c
+++ calculator.c
@@ -1,41 +1,75 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 char display[50] = "0";
+char processText[100] = "";
 char op = 0;
 double num1 = 0;
 int newInput = 1;
 
 HWND hDisplay;
+HWND hProcess;
 
-void Update() { SetWindowText(hDisplay, display); }
+void UpdateDisplay() {
+    SetWindowText(hDisplay, display);
+}
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch (msg)
-    {
-    case WM_CREATE:
-    {
+void UpdateProcess() {
+    SetWindowText(hProcess, processText);
+}
+
+void ClearAll() {
+    strcpy(display, "0");
+    strcpy(processText, "");
+    op = 0;
+    num1 = 0;
+    newInput = 1;
+    UpdateDisplay();
+    UpdateProcess();
+}
+
+double Calculate(double a, double b, char oper) {
+    switch (oper) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return (b != 0) ? a / b : 0;
+        case '%': return (a * b) / 100.0;
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch(msg) {
+
+    case WM_CREATE: {
+        // ช่อง process
+        hProcess = CreateWindow("STATIC", "",
+            WS_VISIBLE | WS_CHILD,
+            20, 5, 260, 20,
+            hwnd, NULL, NULL, NULL);
+
+        // ช่อง display
         hDisplay = CreateWindow("EDIT", "0",
-                                WS_VISIBLE | WS_CHILD | ES_RIGHT,
-                                20, 20, 240, 40, hwnd, NULL, NULL, NULL);
+            WS_VISIBLE | WS_CHILD | ES_RIGHT,
+            20, 25, 260, 40,
+            hwnd, NULL, NULL, NULL);
 
         int x = 20, y = 80;
         char txt[3];
 
         // ปุ่ม 1-9
-        for (int i = 1; i <= 9; i++)
-        {
+        for (int i = 1; i <= 9; i++) {
             sprintf(txt, "%d", i);
             CreateWindow("BUTTON", txt,
-                         WS_VISIBLE | WS_CHILD,
-                         x, y, 60, 40,
-                         hwnd, (HMENU)(100 + i), NULL, NULL);
+                WS_VISIBLE | WS_CHILD,
+                x, y, 60, 40,
+                hwnd, (HMENU)(100 + i), NULL, NULL);
 
             x += 70;
-            if (i % 3 == 0)
-            {
+            if (i % 3 == 0) {
                 x = 20;
                 y += 50;
             }
@@ -43,75 +77,84 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
         // ปุ่ม 0
         CreateWindow("BUTTON", "0",
-                     WS_VISIBLE | WS_CHILD,
-                     90, y, 60, 40,
-                     hwnd, (HMENU)100, NULL, NULL);
+            WS_VISIBLE | WS_CHILD,
+            90, y, 60, 40,
+            hwnd, (HMENU)100, NULL, NULL);
 
         // ปุ่ม =
         CreateWindow("BUTTON", "=",
-                     WS_VISIBLE | WS_CHILD,
-                     160, y, 60, 40,
-                     hwnd, (HMENU)300, NULL, NULL);
+            WS_VISIBLE | WS_CHILD,
+            160, y, 60, 40,
+            hwnd, (HMENU)300, NULL, NULL);
+
+        // ปุ่ม C
+        CreateWindow("BUTTON", "C",
+            WS_VISIBLE | WS_CHILD,
+            20, y, 60, 40,
+            hwnd, (HMENU)400, NULL, NULL);
 
         // Operator
         char ops[] = {'+', '-', '*', '/', '%'};
-        for (int i = 0; i < 5; i++)
-        {
+        for (int i = 0; i < 5; i++) {
             txt[0] = ops[i];
             txt[1] = '\0';
 
             CreateWindow("BUTTON", txt,
-                         WS_VISIBLE | WS_CHILD,
-                         230, 80 + (i * 50), 50, 40,
-                         hwnd, (HMENU)(200 + i), NULL, NULL);
+                WS_VISIBLE | WS_CHILD,
+                230, 80 + (i * 50), 50, 40,
+                hwnd, (HMENU)(200 + i), NULL, NULL);
         }
 
         break;
     }
 
-    case WM_COMMAND:
-    {
+    case WM_COMMAND: {
         int id = LOWORD(wParam);
 
         // ===== ตัวเลข =====
-        if (id >= 100 && id <= 109)
-        {
+        if (id >= 100 && id <= 109) {
             char num = (id - 100) + '0';
 
-            if (newInput || strcmp(display, "0") == 0)
-            {
+            if (newInput || strcmp(display, "0") == 0) {
                 sprintf(display, "%c", num);
                 newInput = 0;
-            }
-            else
-            {
+            } else {
                 int len = strlen(display);
                 display[len] = num;
                 display[len + 1] = '\0';
             }
-
             UpdateDisplay();
         }
 
         // ===== Operator =====
-        if (id >= 200 && id <= 204)
-        {
+        if (id >= 200 && id <= 204) {
             num1 = atof(display);
             op = "+-*/%"[id - 200];
+
+            sprintf(processText, "%.2f %c", num1, op);
+            UpdateProcess();
+
             newInput = 1;
         }
 
         // ===== = =====
-        if (id == 300 && op != 0)
-        {
+        if (id == 300 && op != 0) {
             double num2 = atof(display);
             double result = Calculate(num1, num2, op);
+
+            sprintf(processText, "%.2f %c %.2f =", num1, op, num2);
+            UpdateProcess();
 
             sprintf(display, "%.2f", result);
             UpdateDisplay();
 
             newInput = 1;
             op = 0;
+        }
+
+        // ===== C =====
+        if (id == 400) {
+            ClearAll();
         }
 
         break;
@@ -124,21 +167,26 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
     return DefWindowProc(hwnd, msg, wParam, lParam);
 }
-int WINAPI WinMain(HINSTANCE h, HINSTANCE p, LPSTR a, int s)
-{
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR args, int show) {
+
     WNDCLASS wc = {0};
     wc.lpfnWndProc = WndProc;
-    wc.hInstance = h;
-    wc.lpszClassName = "CalcV2";
+    wc.hInstance = hInst;
+    wc.lpszClassName = "CalcV5";
+
     RegisterClass(&wc);
-    CreateWindow("CalcV2", "Calculator V2",
-                 WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-                 200, 100, 320, 350, NULL, NULL, h, NULL);
-    MSG m;
-    while (GetMessage(&m, NULL, 0, 0))
-    {
-        TranslateMessage(&m);
-        DispatchMessage(&m);
+
+    CreateWindow("CalcV5", "Calculator V5",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        200, 100, 330, 400,
+        NULL, NULL, hInst, NULL);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
+
     return 0;
 }
